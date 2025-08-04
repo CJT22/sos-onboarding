520abe8f559d386922d43064a2a3435b4951a2c6
Chester Jonathan Tayag <tayagchesterjonathan@gmail.com>
2025-08-04 21:15:12 +0800

    Final Exercise: Polish & Testing

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,16 +1,19 @@
 import { Stack } from "expo-router";
 import { StatusBar } from "expo-status-bar";
 import QueryProvider from "../providers/QueryProvider";
+import ErrorBoundary from "../components/ErrorBoundary";
 
 export default function RootLayout() {
   return (
-    <QueryProvider>
-      <Stack>
-        <Stack.Screen name="index" options={{ headerShown: false }} />
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="(auth)" options={{ headerShown: false }} />
-      </Stack>
-      <StatusBar style="auto" />
-    </QueryProvider>
+    <ErrorBoundary>
+      <QueryProvider>
+        <Stack>
+          <Stack.Screen name="index" options={{ headerShown: false }} />
+          <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+          <Stack.Screen name="(auth)" options={{ headerShown: false }} />
+        </Stack>
+        <StatusBar style="auto" />
+      </QueryProvider>
+    </ErrorBoundary>
   );
 }
